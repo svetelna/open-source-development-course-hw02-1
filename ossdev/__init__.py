c9252f1189875d583387219f87a3e06876e178cc
svetelna <zu.miklankova@gmail.com>
2020-03-05 15:14:54 +0100

    implemented vector operations

--- ossdev/__init__.py
+++ ossdev/__init__.py
@@ -1,6 +1,6 @@
 # Useful doc on Python magic methods:
 # https://rszalski.github.io/magicmethods/
-
+import math
 
 class Vector:
     def __init__(self, arr=None, size=None):
@@ -34,19 +34,19 @@ class Vector:
         return sum(self.d)
 
     def __setitem__(self, key, value):
-        # TODO: implement
-        return None
+        self.d[key] = value #done
+        return None	
 
     def __cmp__(self, other):
-        # TODO: implement, -1 if self < other, 0 if self == other, 1 if self > other
-        return -1
+        if self == other:
+            return 0
+        return -1 if self < other else 1
 
     def __neg__(self):
         return Vector([-x for x in self.d])
 
     def __reversed__(self):
-        # TODO: implement vector element reversal (hint: list(reversed(self.d)))
-        return Vector()
+        return list(reversed(self.d))
 
     def __add__(self, other):
         if isinstance(other, int):
@@ -55,17 +55,14 @@ class Vector:
             return Vector([self.d[i] + other[i] for i in range(len(self))])
 
     def __sub__(self, other):
-        # TODO: implement vector subtraction
-        return None
+        return list( (a-b for a,b in zip(self.d, other.d) ) )
 
     def __mul__(self, other):
-        # TODO: implement vector multiplication by a scalar value
-        return None
+        return list( (a*other for a in self.d))
 
     def __xor__(self, other):
-        # TODO: implement bit-wise XOR with a scalar value
-        return None
+        return list( (a^other for a in self.d))
 
     def length(self):
-        # TODO: implement vector length comp. (hint: return math.sqrt(sum(x*x for x in self.d)))
-        return None
+        return math.sqrt(sum(x*x for x in self.d))
+
